9757e764b8865acf29808947da921740900ddbdd
anoju <zun5761@gmail.com>
2023-03-23 10:57:45 +0900

    test

---
+++ ahj/js/test.js
@@ -0,0 +1,16 @@
+// get random number
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// get random color
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
