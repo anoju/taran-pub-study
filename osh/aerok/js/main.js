ecbed94cf31954916c4a6af78d5919b99a0b868e
sunhwa oh <ohsh0613@naver.com>
2023-04-10 23:21:04 +0900

    230410

--- osh/aerok/js/main.js
+++ osh/aerok/js/main.js
@@ -7,6 +7,7 @@ $(function () {
 
 function mainInit() {
   event();
+  toggleSlide();
 }
 
 function event() {
@@ -34,4 +35,43 @@ function event() {
   //     swiper.autoplay.start();
   //   }
   // );
-}
+} //end event
+
+function toggleSlide() {
+  let curPos = 0;
+  let postion = 0;
+  let start_x, end_x;
+  const IMAGE_WIDTH = 280;
+  const images = document.querySelector('.slide1');
+
+  images.addEventListener('touchstart', touch_start);
+  images.addEventListener('touchend', touch_end);
+
+  function prev() {
+    if (curPos > 0) {
+      postion += IMAGE_WIDTH;
+      images.style.transform = `translateX(${postion}px)`;
+      curPos = curPos - 1;
+    }
+  }
+  function next() {
+    if (curPos < 3) {
+      postion -= IMAGE_WIDTH;
+      images.style.transform = `translateX(${postion}px)`;
+      curPos = curPos + 1;
+    }
+  }
+
+  function touch_start(event) {
+    start_x = event.touches[0].pageX;
+  }
+
+  function touch_end(event) {
+    end_x = event.changedTouches[0].pageX;
+    if (start_x > end_x) {
+      next();
+    } else {
+      prev();
+    }
+  }
+} //end toggleSlide
