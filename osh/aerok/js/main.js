285d08d8b93e361ab664e1a67d854fd71383a485
Sunhwa Oh <ohsh0613@naver.com>
2023-04-11 14:04:12 +0900

    230411

--- osh/aerok/js/main.js
+++ osh/aerok/js/main.js
@@ -6,12 +6,12 @@ $(function () {
 });
 
 function mainInit() {
-  event();
+  mainBanner();
   toggleSlide();
 }
 
-function event() {
-  var swiper = new Swiper('.event', {
+function mainBanner() {
+  var mainSwiper = new Swiper('.main-banner', {
     scrollbar: {
       el: '.swiper-scrollbar',
       hide: false
@@ -27,51 +27,30 @@ function event() {
     loop: true
   });
 
-  // $('.swiper-slide').hover(
-  //   function () {
-  //     swiper.autoplay.stop();
-  //   },
-  //   function () {
-  //     swiper.autoplay.start();
-  //   }
-  // );
-} //end event
+  //main-banner 정지 재생 버튼
+  $('.swiper-auto-btn').click(function (e) {
+    e.preventDefault();
+    const $this = $(this);
+    const $stopImg = 'images/icon_pause.png';
+    const $playImg = 'images/icon_stop.png';
+    if tt($this.hasClass('playing')) {
+      mainSwiper.autoplay.stop();
+      $this.removeClass('playing').find('img').attr('src', $playImg);
+    } else {
+      mainSwiper.autoplay.start();
+      $this.addClass('playing').find('img').attr('src', $stopImg);
+    }
+  });
+}
 
 function toggleSlide() {
-  let curPos = 0;
-  let postion = 0;
-  let start_x, end_x;
-  const IMAGE_WIDTH = 280;
-  const images = document.querySelector('.slide1');
-
-  images.addEventListener('touchstart', touch_start);
-  images.addEventListener('touchend', touch_end);
-
-  function prev() {
-    if (curPos > 0) {
-      postion += IMAGE_WIDTH;
-      images.style.transform = `translateX(${postion}px)`;
-      curPos = curPos - 1;
-    }
-  }
-  function next() {
-    if (curPos < 3) {
-      postion -= IMAGE_WIDTH;
-      images.style.transform = `translateX(${postion}px)`;
-      curPos = curPos + 1;
-    }
-  }
-
-  function touch_start(event) {
-    start_x = event.touches[0].pageX;
-  }
-
-  function touch_end(event) {
-    end_x = event.changedTouches[0].pageX;
-    if (start_x > end_x) {
-      next();
-    } else {
-      prev();
-    }
-  }
-} //end toggleSlide
+  var swiper = new Swiper('.toggle-slide', {
+    breakpoints: {
+      320: {
+        slidesPerView: 1.3,
+        spaceBetween: 20
+      }
+    },
+    loop: true
+  });
+}
