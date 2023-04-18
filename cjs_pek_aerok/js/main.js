800306605ec16471ef30c1d24068381afe4e3801
js choi <photosynthesis0131@gmail.com>
2023-04-18 13:13:12 +0900

    2023-04-18

--- cjs_pek_aerok/js/main.js
+++ cjs_pek_aerok/js/main.js
@@ -1,1 +1,20 @@
-console.log('hi');
+$(document).ready(function () {
+  function swipe() {
+    const swiper2 = new Swiper('.swiper', {
+      slidesPerView: 'auto',
+      grabCursor: true,
+      effect: 'creative',
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: ['-100%', 0, -1]
+        },
+        next: {
+          translate: ['100%', 0, 0]
+        }
+      }
+    });
+  } //swipe
+
+  swipe();
+}); //document.ready
