eb7e0e5d9a8b51b05c34ebc5b523826bf046991d
js choi <photosynthesis0131@gmail.com>
2023-04-18 18:31:08 +0900

    2023-0418

--- cjs_pek_aerok/js/main.js
+++ cjs_pek_aerok/js/main.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
+
   function swipe() {
-    const swiper2 = new Swiper('.swiper', {
-      slidesPerView: 'auto',               
-      effect: 'creative',
+    const swiper1 = new Swiper('.swiper', {
+      slidesPerView: 'auto',     
+      loop: true,          
+      effect: 'creative',  
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
       creativeEffect: {
         prev: {
           translate: ['0%', 0, 0],
@@ -10,9 +16,20 @@ $(document).ready(function () {
         next: {
           translate: ['100%', 0, 1],
         },
-      },
-      
-    });
+      },    
+      on: {
+        init : function () {    
+          $('.swiper-pagination-current').text('01')
+          $('.swiper-pagination-total').text('04')    
+        },
+        slideChange : function() {    
+          let currentNum = $('.swiper-pagination-current').text()
+          let totalNum = $('.swiper-pagination-total').text()           
+          $('.swiper-pagination-current').text('0' + currentNum)
+          $('.swiper-pagination-total').text('0' + totalNum)            
+        },
+      },       
+    });   
   } //swipe
 
   swipe();
