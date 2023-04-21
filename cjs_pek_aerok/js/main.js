04cafdc6117305a0260d7ee13c5c4bc66b528914
EunKyoP <xoxo.popeye77@gmail.com>
2023-04-21 09:24:04 +0900

    230421 - mainswiper z-index 수정

--- cjs_pek_aerok/js/main.js
+++ cjs_pek_aerok/js/main.js
@@ -1,76 +1,86 @@
 $(document).ready(function () {
   //최재석
 
-  function scrollActionBanner(){
-    $(window).on('scroll', function() {
+  function scrollActionBanner() {
+    $(window).on('scroll', function () {
       const boxOffset = $('.letter_box').offset().top;
       const y = $(window).height() / 3;
       const x = $(window).scrollTop();
-      
-      console.log(x+y, boxOffset) 
-      if(x+y > boxOffset)  {     
-        $('.letter_box').stop().animate({backgroundSize: `${100 + parseInt(x / 100, 0)}%`}, 400);
-        $('.letter_box p').stop().animate({fontSize: parseInt(x / 180, 0)}, 400);      
-      }else{
-        $('.letter_box').stop().animate({backgroundSize: `${100 + parseInt(x / 200, 0)}%`}, 400);      
+
+      //console.log(x+y, boxOffset)
+      if (x + y > boxOffset) {
+        $('.letter_box')
+          .stop()
+          .animate({ backgroundSize: `${100 + parseInt(x / 100, 0)}%` }, 400);
+        $('.letter_box p')
+          .stop()
+          .animate({ fontSize: parseInt(x / 180, 0) }, 400);
+      } else {
+        $('.letter_box')
+          .stop()
+          .animate({ backgroundSize: `${100 + parseInt(x / 200, 0)}%` }, 400);
       }
     });
   }
- 
+
   function mainSwipe() {
     const swiper1 = new Swiper('.main-swiper', {
-      slidesPerView: 'auto',     
-      loop: true,          
-      effect: 'creative',  
+      slidesPerView: 'auto',
+      loop: true,
+      effect: 'creative',
       pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
+        el: '.swiper-pagination',
+        type: 'fraction'
       },
       creativeEffect: {
         prev: {
-          translate: ['0%', 0, 0],
+          translate: ['0%', 0, -1]
         },
         next: {
-          translate: ['100%', 0, 1],
-        },
-      },    
+          translate: ['100%', 1, 0]
+        }
+      },
       on: {
-        init : function () {    
-          $('.swiper-pagination-current').text('01')
-          $('.swiper-pagination-total').text('04')    
+        init: function () {
+          $('.swiper-pagination-current').text('01');
+          $('.swiper-pagination-total').text('04');
         },
-        slideChange : function() {    
-          let currentNum = $('.swiper-pagination-current').text()
-          let totalNum = $('.swiper-pagination-total').text()           
-          $('.swiper-pagination-current').text('0' + currentNum)
-          $('.swiper-pagination-total').text('0' + totalNum)            
-        },
-      },       
-    });   
+        slideChange: function () {
+          let currentNum = $('.swiper-pagination-current').text();
+          let totalNum = $('.swiper-pagination-total').text();
+          $('.swiper-pagination-current').text('0' + currentNum);
+          $('.swiper-pagination-total').text('0' + totalNum);
+        }
+      }
+    });
   } //swipe
 
-  function subSwipe(){
-    const swiper2 = new Swiper('.swiper', {       
+  function subSwipe() {
+    const swiper2 = new Swiper('.swiper', {
       slidesPerView: 'auto',
       spaceBetween: 20,
       slidesOffsetBefore: 24,
       slidesOffsetAfter: 24
-    })
+    });
   } //subswipe
 
-  function selectBox(){    
+  function selectBox() {
     $('select[name=recommend]').change(function () {
       const val = $(this).val();
-      const option = $(this).find($('option[value=' + val + ']')).attr('class');
-      $('#' + option).addClass('active').siblings().removeClass('active')
+      const option = $(this)
+        .find($('option[value=' + val + ']'))
+        .attr('class');
+      $('#' + option)
+        .addClass('active')
+        .siblings()
+        .removeClass('active');
     });
   } //selectBox
 
   mainSwipe();
   subSwipe();
   selectBox();
-  scrollActionBanner()
+  scrollActionBanner();
 
   // 박은교
-
 }); //document.ready
